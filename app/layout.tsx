f080699cac1ab8fc8f87dd5f80b94be4e69ccf2e
Asharib <asharibarain4@gmail.com>
2024-09-28 10:42:53 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Edu Dapp | Starter-Kit",
-  description: "A full stack starter template with the CLI for developers to launch dApps on EDU Chain.",
+  description:
+    "A full stack starter template with the CLI for developers to launch dApps on EDU Chain.",
 };
 
 export default function RootLayout({
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
