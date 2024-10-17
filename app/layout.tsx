0c12106a58c9892ed56612b39b4e174023d4a9f3
Asharib <asharibarain4@gmail.com>
2024-10-17 12:42:42 +0500

    update google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-      </body>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-L694N0QK6B"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+   
+   gtag('config', 'G-L694N0QK6B');
+   `}
+      </Script>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
